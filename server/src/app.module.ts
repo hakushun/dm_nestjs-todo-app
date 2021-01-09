446226bb0b6e7fe0a1135f30ce32de015cdf3328
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-09 17:58:04 +0900

    refactor: auth, typeORM関連のファイルと記述の削除

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
 import { TodosModule } from './todos/todos.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-	imports: [TypeOrmModule.forRoot(typeOrmConfig), TodosModule, AuthModule],
+	imports: [TodosModule],
 })
 export class AppModule {}
--- server/src/todos/todos.service.ts
+++ server/src/todos/todos.service.ts
@@ -1,54 +1,30 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { TodoStatus } from './todo-status';
-import { GetTodosFilterDto } from './dto/get-todos-filter.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { TodoRepository } from './todo.repository';
 import { Todo } from './todo.entity';
-import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class TodosService {
-	constructor(
-		@InjectRepository(TodoRepository)
-		private todoRepository: TodoRepository,
-	) {}
-
-	async getTodos(filterDto: GetTodosFilterDto, user: User): Promise<Todo[]> {
-		return this.todoRepository.getTodos(filterDto, user);
+	async getTodos(): Promise<Todo[]> {
+		return;
 	}
 
-	async getTodoById(id: number, user: User): Promise<Todo> {
-		const target = await this.todoRepository.findOne({
-			where: { id, userId: user.id },
-		});
-
-		if (!target) {
-			throw new NotFoundException();
-		}
-		return target;
+	async getTodoById(id: number): Promise<Todo> {
+		return;
 	}
 
-	async createTodo(todo: CreateTodoDto, user: User): Promise<Todo> {
-		return this.todoRepository.createTodo(todo, user);
+	async createTodo(todo: CreateTodoDto): Promise<Todo> {
+		return;
 	}
 
-	async deleteTodo(id: number, user: User): Promise<void> {
-		const result = await this.todoRepository.delete({ id, userId: user.id });
-
-		if (result.affected === 0) {
-			throw new NotFoundException();
-		}
+	async deleteTodo(id: number): Promise<void> {
+		return;
 	}
 
 	async updateTodoStatus(
 		id: number,
 		status: TodoStatus,
-		user: User,
 	): Promise<Todo> {
-		const todo = await this.getTodoById(id, user);
-		todo.status = status;
-		await todo.save();
-		return todo;
+		return;
 	}
 }
