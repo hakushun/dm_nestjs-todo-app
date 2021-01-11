75a7a6d1413fef69e16caf33476bae8bf637ca0e
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-11 14:55:20 +0900

    feat: CRUDメソッド実装

--- server/src/todos/todos.service.ts
+++ server/src/todos/todos.service.ts
@@ -1,30 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { TodoStatus } from './todo-status';
 import { Todo } from './todo.entity';
+import { knex } from '../../db/knex';
 
 @Injectable()
 export class TodosService {
 	async getTodos(): Promise<Todo[]> {
-		return;
+		return knex.select().from('todos');
 	}
 
 	async getTodoById(id: number): Promise<Todo> {
-		return;
+		const target = await knex
+			.select()
+			.from('todos')
+			.where('id', id);
+
+		if (target.length === 0) {
+			throw new NotFoundException();
+		}
+
+		return target;
 	}
 
 	async createTodo(todo: CreateTodoDto): Promise<Todo> {
-		return;
+		const id = await knex('todos')
+			.returning('id')
+			.insert({ title: todo.title, detail: todo.detail });
+		return knex
+			.select()
+			.from('todos')
+			.where('id', id[0]);
 	}
 
 	async deleteTodo(id: number): Promise<void> {
+		await this.getTodoById(id);
+		await knex('todos')
+			.where('id', id)
+			.del();
 		return;
 	}
 
-	async updateTodoStatus(
-		id: number,
-		status: TodoStatus,
-	): Promise<Todo> {
-		return;
+	async updateTodoStatus(id: number, status: TodoStatus): Promise<Todo> {
+		await this.getTodoById(id);
+		await knex('todos')
+			.where('id', id)
+			.update({ status });
+		return knex('todos').where('id', id);
 	}
 }
