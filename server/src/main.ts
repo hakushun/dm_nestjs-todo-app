37ac06b02975f7d141f475911ddf8e72fdc2bb5c
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-03-07 14:16:35 +0900

    feat: middlewareの初期化処理追加

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import * as helmet from 'helmet';
+import * as session from 'express-session';
+import * as express from 'express';
+import * as csurf from 'csurf';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest-express-application.interface';
 import { Logger } from '@nestjs/common';
+import { join } from 'path';
 require('dotenv').config();
 
 async function bootstrap() {
@@ -15,6 +19,22 @@ async function bootstrap() {
 		app.use(helmet());
 		app.enableCors({ origin: process.env.CORS_ORIGIN });
 	}
+
+	app.use(
+		session({
+			secret: 'secret',
+			resave: false,
+			saveUninitialized: false,
+		}),
+	);
+
+	app.use(express.urlencoded({ extended: false }));
+	app.use(csurf({ cookie: false }));
+
+	// dist/views配下に配置
+	app.useStaticAssets(join(__dirname, '..', 'public'));
+	app.setBaseViewsDir(join(__dirname, '..', 'views'));
+	app.setViewEngine('hbs');
 
 	const port = process.env.PORT;
 	await app.listen(port);
