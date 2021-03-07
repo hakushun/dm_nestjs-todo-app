3a7ceb55519d94c7e4e9f5231302a85a166a3b76
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-03-07 16:06:01 +0900

    feat: AuthModuleの呼び出し

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TodosModule } from './todos/todos.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-	imports: [TodosModule],
+	imports: [TodosModule, AuthModule],
 	controllers: [AppController],
 })
 export class AppModule {}
