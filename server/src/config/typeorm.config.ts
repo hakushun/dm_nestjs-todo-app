2d19cff36ef3c225a7d604bf3f5157e6d2817da3
Shun Nakano <nakano_syun@waku-2.com>
2021-03-11 09:27:29 +0900

    refactor: typeOrmにssl設定追加

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -10,4 +10,5 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
 	database: process.env.RDS_DB_NAME || process.env.DB_NAME,
 	entities: [__dirname + '/../**/*.entity.{js,ts}'],
 	synchronize: process.env.TYPEORM_SYNC === 'false' ? false : true,
+	ssl: { rejectUnauthorized : false },
 };
